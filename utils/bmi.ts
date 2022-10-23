eaffbac03f5ec53b18c11fea479810bd9e151b84
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-10-23 17:48:47 +0530

    Prettier integration

--- utils/bmi.ts
+++ utils/bmi.ts
@@ -1,27 +1,24 @@
-import  {  bmiChart,  } from "../config/bmi.config";
-import { BMIRecord, Condition, FactOperator} from '../types'
-
+import { bmiChart } from '../config/bmi.config';
+import { BMIRecord, Condition, FactOperator } from '../types';
 
 export const computeBMI = (height: number, weight: number): number => {
-    return weight / (height**2)
-}
+  return weight / height ** 2;
+};
 
+export const getBMIAnalysis = (bmi: number): BMIRecord | void => {
+  const isValid = isValidFact.bind(null, bmi);
+  return bmiChart.find((chart) => chart.rangeFacts.every(isValid));
+};
 
-export const getBMIAnalysis = (bmi: number) : BMIRecord | void => {
-    const isValid = isValidFact.bind(null, bmi)
-    return bmiChart.find(chart => chart.rangeFacts.every(isValid))
-}
-
-const isValidFact = (bmi :number, condition :Condition) : boolean => {
-	let isValid : boolean
-	switch (condition.operator) {
-    	case FactOperator.GreaterThanOrEqual:
-		    isValid = bmi >= condition.value
-            break;
-	    case FactOperator.LessThanOrEqual:
-		    isValid = bmi <= condition.value
-            break;
-	}
-	return isValid
-}
-
+const isValidFact = (bmi: number, condition: Condition): boolean => {
+  let isValid: boolean;
+  switch (condition.operator) {
+    case FactOperator.GreaterThanOrEqual:
+      isValid = bmi >= condition.value;
+      break;
+    case FactOperator.LessThanOrEqual:
+      isValid = bmi <= condition.value;
+      break;
+  }
+  return isValid;
+};

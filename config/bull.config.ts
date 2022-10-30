df7d258668107944c2dd6f6649a05223dabf9c47
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-10-30 20:02:48 +0530

    feat: :technologist: update job progress when finished

--- config/bull.config.ts
+++ config/bull.config.ts
@@ -1,10 +1,19 @@
 import { Worker, Processor, ConnectionOptions, Queue, Job } from 'bullmq';
 import { logger } from './log.config';
-import { connection } from './redis.config';
+import { getConnection } from './redis.config';
 
 const concurrency = +(process.env.CONCURRENT_WORKERS || 1);
 
-export const createQueue = <T>(name: string) => new Queue<T>(name, { connection });
+export const createQueue = <T>(name: string) =>
+  new Queue<T>(name, {
+    connection: getConnection(),
+    // defaultJobOptions: {
+    //   removeOnComplete: {
+    //     age: 3600, // keep up to 1 hour
+    //     count: 50, // keep up to 50 jobs
+    //   },
+    // },
+  });
 
 export function createWorker<T>(name: string, processor: Processor, connection: ConnectionOptions) {
   const worker = new Worker(name, processor, {
@@ -19,6 +28,14 @@ export function createWorker<T>(name: string, processor: Processor, connection: 
     }
     if (job.id) {
       logger.info(`Completed job ${job.id} on queue ${name}`);
+      job
+        .updateProgress(100)
+        .then(() => {
+          logger.info('update progress');
+        })
+        .catch((err) => {
+          logger.error('err', err);
+        });
     }
   });
 
